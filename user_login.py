caf5ca26d53f61049be1904fd90dce71035731b8
ren-jamie11 <ren.jamie11@gmail.com>
2026-03-09 17:21:21 +0800

    more passwords

--- user_login.py
+++ user_login.py
@@ -7,4 +7,6 @@ USER_PASSWORDS = {
     st.secrets["PASS_GUO"]: "郭嘉俊",
     st.secrets["PASS_ABBY"]: "Abby",
     st.secrets["PASS_TAO"]: "小桃",
+    st.secrets["PASS_YAN"]: "袁燕苗",
+    st.secrets["PASS_SOPHIE"]: "Sophie",
 }
